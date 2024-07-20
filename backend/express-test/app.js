733a533a7a23a47d6c8955a04ed172b5ac79dcb4
Ayush <21ucs044@lnmiit.ac.in>
2024-07-20 08:05:48 +0530

    back to orignal

--- backend/express-test/app.js
+++ backend/express-test/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
-require('dotenv').config(); 
-const app = express();
-const port = process.env.PORT || 3000;
+const cors = require('cors');
+const { server, app } = require('./Socket/socket'); 
+const rootRouter = require('./routes/index'); 
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1', rootRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
