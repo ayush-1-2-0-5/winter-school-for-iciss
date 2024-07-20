ae2610b09d05f9aaf28d0d34f7d948697028064b
Ayush <21ucs044@lnmiit.ac.in>
2024-07-20 08:02:00 +0530

    test

--- backend/express-test/app.js
+++ backend/express-test/app.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-const { server, app } = require('./Socket/socket'); 
-const rootRouter = require('./routes/index'); 
-
-app.use(cors());
-app.use(express.json());
-app.use('/api/v1', rootRouter);
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello World');
 });
 
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-module.exports = app;
