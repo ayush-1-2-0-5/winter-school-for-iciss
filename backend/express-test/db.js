f000a7129d56d74840f016b9d2a48e9d76dc10d5
Ayush <21ucs044@lnmiit.ac.in>
2024-07-20 10:36:26 +0530

    back to orignal

--- backend/express-test/db.js
+++ backend/express-test/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config(); 
 mongoose.connect(
-  process.env.MONGODB_URI,
+  process.env.MONGODB_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
